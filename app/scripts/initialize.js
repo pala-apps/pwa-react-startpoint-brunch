efabae906e3cd1831a617d92960e1b9066e53342
Jay Chetty <jaypaulchetty@gmail.com>
2016-08-31 14:45:51 +0100

    add redux

--- app/scripts/initialize.js
+++ app/scripts/initialize.js
@@ -1,56 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import swi from './service_worker_init'
+swi()
 
-var isLocalhost = Boolean(window.location.hostname === 'localhost' ||
-  // [::1] is the IPv6 localhost address.
-  window.location.hostname === '[::1]' ||
-  // 127.0.0.1/8 is considered localhost for IPv4.
-  window.location.hostname.match(
-    /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-  )
-);
+import React from 'react';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import dummyReducer  from './reducers'
+import DummyContainer from './components/DummyContainer'
+import { Provider } from 'react-redux'
+let store = createStore(dummyReducer);
 
-if ('serviceWorker' in navigator &&
-    (window.location.protocol === 'https:' || isLocalhost)) {
-  navigator.serviceWorker.register('sw.js')
-  .then(function(registration) {
-    console.log('in service worker setup')
-    // updatefound is fired if service-worker.js changes.
-    registration.onupdatefound = function() {
-      // updatefound is also fired the very first time the SW is installed,
-      // and there's no need to prompt for a reload at that point.
-      // So check here to see if the page is already controlled,
-      // i.e. whether there's an existing service worker.
-      if (navigator.serviceWorker.controller) {
-        // The updatefound event implies that registration.installing is set:
-        // https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-container-updatefound-event
-        var installingWorker = registration.installing;
 
-        installingWorker.onstatechange = function() {
-          switch (installingWorker.state) {
-            case 'installed':
-              // At this point, the old content will have been purged and the
-              // fresh content will have been added to the cache.
-              // It's the perfect time to display a "New content is
-              // available; please refresh." message in the page's interface.
-              break;
-
-            case 'redundant':
-              throw new Error('The installing ' +
-                              'service worker became redundant.');
-
-            default:
-              // Ignore
-          }
-        };
-      }
-    };
-  }).catch(function(e) {
-    console.error('Error during service worker registration:', e);
-  });
-}
-
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
+render(
+  <Provider store={store}>
+    <DummyContainer/>
+  </Provider>,
   document.getElementById('app')
 );
